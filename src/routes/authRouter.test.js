9a2d261bcc73854deeb99b9e2745e6d28cc87619
ivan926 <navis462@gmail.com>
2024-10-07 15:35:19 -0600

    Adding franchise router test and order router, along with workflows file for github actions,services is currently being highlighted as being an error for now?

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,15 +1,138 @@
 const request = require("supertest")
 const app = require("../service.js")
 
-test("Test now ",() => {
-    expect('hello there').toBe("hello there");
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken = null;
+let userID = null;
 
-});
+beforeAll(async () => {
+    testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+
+    const badTestUser = { name: 'pizza diner', email: '', password: '' };
+    const wrongResponse = await request(app).post('/api/auth').send(badTestUser);
+    expect(wrongResponse.status).toBe(400);
+
+    const registerRes = await request(app).post('/api/auth').send(testUser);
+    testUserAuthToken = registerRes.body.token;
+  });
+  
+  test('login', async () => {
+    const loginRes = await request(app).put('/api/auth').send(testUser);
+    expect(loginRes.status).toBe(200);
+    expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+  
+    const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+    expect(loginRes.body.user).toMatchObject(user);
+    //obligated lint checker
+    expect(testUser.password).toBe(password);
+
+   userID = loginRes.body.user.id;
+
+   //test bad token 
+
+    
+  });
+
+  test("update user",async ()=>{
 
 
-test("Test function, dummy",()=> {
+    
+   let newEmail = testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+   let newUser = {email:`${newEmail}`, password:`${testUser.password}`};
+   const loginRes = await request(app).put(`/api/auth/${userID}`).set('Authorization', `Bearer ${testUserAuthToken}`).send(newUser)
 
-    let object = null;
-    expect(object).toBeDefined();
+   const WrongIDRes = await request(app).put(`/api/auth/wrong`).set('Authorization', `Bearer ${testUserAuthToken}`).send(newUser)
+    expect(WrongIDRes.status).toBe(403);
 
-})
+
+
+
+
+   expect(loginRes.body.email).toBe(newEmail);
+
+
+
+  })
+
+
+
+  test('logout', async () => {
+    
+    const loginRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`)
+    .send(testUser);
+    
+    expect(loginRes.status).toBe(200);
+
+    expect(loginRes.text).toBe(`{"message":"logout successful"}`);
+
+
+    
+  });
+
+  //these are the first endpoints
+  test('Unknown point', async () => {
+    
+    const loginRes = await request(app).post('/api/auth/unknown').set('Authorization', `Bearer ${testUserAuthToken}`)
+    .send(testUser);
+    
+    expect(loginRes.status).toBe(404);
+
+    expect(loginRes.body.message).toBe('unknown endpoint')
+
+
+
+
+    
+  });
+
+   //these are the first endpoints
+   test('Welcome to JWT', async () => {
+    
+    const loginRes = await request(app).get('/')
+    
+    expect(loginRes.status).toBe(200);
+
+    expect(loginRes.body.message).toBe('welcome to JWT Pizza')
+
+    expect(loginRes.body.version).toBe("20240518.154317");
+
+
+
+
+    
+  });
+
+
+   //these are the first endpoints
+   test('Docs', async () => {
+    
+    const loginRes = await request(app).get('/api/docs')
+
+    //console.log(loginRes.body)
+    
+    expect(loginRes.status).toBeDefined();
+
+
+
+
+
+    
+  });
+
+
+   test('Unauthorized dealer', async () => {
+    
+
+    let newEmail = testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+    let newUser = {email:`${newEmail}`, password:`${testUser.password}`};
+    const loginRes = await request(app).put(`/api/auth/${userID}`).set('Authorization', `Bearer sdsdwer`).send(newUser)
+ 
+    expect(loginRes.body.message).toBe("unauthorized");
+
+
+
+
+
+    
+  });
+
---
+++ src/routes/orderRouter.test.js
@@ -0,0 +1,119 @@
+const request = require("supertest")
+const app = require("../service.js")
+
+
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+
+let testUserAuthToken = null;
+let userID = null;
+let adminUserAuthToken = null;
+
+
+beforeAll(async () => {
+    testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+   
+    //login as admin
+    let adminRegisterRes = await request(app).put('/api/auth').send({"email":"a@jwt.com", "password":"admin"});
+    adminUserAuthToken = adminRegisterRes.body.token;
+
+    //register new user and use his auth token probably overkill, definitely actually
+    const registerRes = await request(app).post('/api/auth').send(testUser);
+    testUserAuthToken = registerRes.body.token;
+    userID = registerRes.body.id;
+
+    
+  });
+  
+
+  test("get pizza menu",async ()=>{
+
+
+    const loginRes = await request(app).get(`/api/order/menu`);
+    expect(loginRes.body).toBeDefined();
+
+  })
+
+
+  test("Unsuccessfully Add an item to menu",async ()=>{
+
+    let newItemOnMenu = { "title":"Student", "description": "Everything on the burger please", "image":"nunya.png", "price": 0.0001 }
+  
+    const menuResponse = await request(app).put("/api/order/menu").set('Authorization', `Bearer ${testUserAuthToken}`).send(newItemOnMenu);
+    
+    expect(menuResponse.statusCode).toBe(403)
+
+
+   
+   })
+
+   test("Successfully add an item to the menu ",async ()=>{
+
+    //current admin user is not working so using another admin account to sign in
+    let tempUserAuthorizedToSignIn = await request(app).put('/api/auth').send({"email":"j12ehy3v3p@admin.com", "password":"toomanysecrets"});
+    let tempToken = tempUserAuthorizedToSignIn.body.token;
+   // console.log(tempUserAuthorizedToSignIn.body);
+
+    let newItemOnMenu = { "title":"Student", "description": "Everything on the burger please", "image":"nunya.png", "price": 0.0001 }
+       //below covers admin login portion for the particular function
+       const correctMenuResponse = await request(app).put("/api/order/menu").set('Authorization', `Bearer ${tempToken}`).send(newItemOnMenu);
+ 
+       expect(correctMenuResponse.statusCode).toBe(200);
+     
+
+       let logOutSuccessResponse = await request(app).delete('/api/auth').set("Authorization", `Bearer ${tempToken}`);
+       expect(logOutSuccessResponse.body.message).toBe('logout successful' );
+
+   })
+
+
+   test('Create an order', async () => {
+
+   
+    let newOrder = {"franchiseId": 2, "storeId":1, "items":[{ "menuId": 1, "description": "Veggie", "price": 0.05 }]}
+    //good response
+    const loginRes = await request(app).post('/api/order').set('Authorization', `Bearer ${testUserAuthToken}`).send(newOrder)
+
+
+    expect(loginRes.body.order.franchiseId).toBe(2)
+
+    let badOrder = {"franchiseId": 2, "storeId":1,"Nah":"bad","this one has to be bad":null, "items":[{ "menuId": null, "description": "Veggie", "price": 0.05 }]}
+   
+    //Not authorized response
+    const badRes = await request(app).post('/api/order').set('Authorization', `Bearer BADAUTH?`).send(newOrder)
+
+    expect(badRes.status).toBe(401);
+
+    //get 500 error code
+    const badRes500 = await request(app).post('/api/order').set('Authorization', `Bearer ${testUserAuthToken}`).send(badOrder);
+
+    expect(badRes500.statusCode).toBe(500);
+
+    // let logOutSuccessResponse = await request(app).delete('/api/auth').set("Authorization", `Bearer ${tempToken}`);
+    // expect(logOutSuccessResponse.body.message).toBe('logout successful' );
+    
+  });
+
+
+  test("get order from auth user",async ()=>{
+
+  
+    const menuResponse = await request(app).get("/api/order").set('Authorization', `Bearer ${testUserAuthToken}`)
+    expect(menuResponse.body.dinerID).toBe(userID);
+ 
+ 
+   })
+
+  
+
+
+  afterAll(async()=>{
+    // //log out of admin account
+    let logOutSuccessResponse = await request(app).delete('/api/auth').set("Authorization", `Bearer ${adminUserAuthToken}`);
+    expect(logOutSuccessResponse.body.message).toBe('logout successful' );
+
+    let normalUserLogOutSuccessResponse = await request(app).delete('/api/auth').set("Authorization", `Bearer ${testUserAuthToken}`);
+    expect(normalUserLogOutSuccessResponse.body.message).toBe('logout successful' );
+ 
+
+  });
+
