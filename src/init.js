d7a6fb2b251a6c5e18d38c56131ef759f34786b1
ivan926 <navis462@gmail.com>
2024-11-13 11:59:26 -0500

    Made change hope ot works

--- src/init.js
+++ src/init.js
@@ -10,3 +10,5 @@ const email = process.argv[3];
 const password = process.argv[4];
 const user = { name, email, password, roles: [{ role: Role.Admin }] };
 DB.addUser(user).then((r) => console.log('created user: ', r));
+
+//change made
